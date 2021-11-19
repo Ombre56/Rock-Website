0e0f94d9e583aa0fdc86f84e7efda7c66a094190
Ombre <krys1014@wp.pl>
2021-11-19 20:19:42 +0100

    Add react-router-dom

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './style.css';
+import { NavLink } from 'react-router-dom';
 
+const list = [
+    { name: "Home", path: "/", exact: true },
+    { name: "Aktualności", path: "/articles" },
+    { name: "Członkowie", path: "/staff" },
+    { name: "Portfolio", path: "/portfolio" },
+    { name: "Kontakt", path: "/contact" },
+]
 function Navigation() {
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink>
+        </li>
+    ))
     return (
-        <>
-            <a href="#home">Aktualnosci</a>
-            <a href="#staff">• Czlonkowie</a>
-            <a href="#portfolio">• Portfolio •</a>
-            <a href="#contact">Kontakt</a>
-        </>
-    )
+        <nav className="main">
+            <ul>
+                {menu}
+            </ul>
+        </nav>
+    );
 }
 
 export default Navigation
