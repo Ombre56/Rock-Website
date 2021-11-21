c714da705f9bfacfcad005d0b02f995efb81da61
Ombre <krys1014@wp.pl>
2021-11-21 19:20:42 +0100

    Add cosial media to Our Team section

--- src/Staff/Staff.js
+++ src/Staff/Staff.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './style.css';
 import Card1 from '../images/card1.jpg';
-// import * as Icon from 'react-bootstrap-icons';
+import * as Icon from 'react-bootstrap-icons';
 import Card2 from '../images/card2.jpg';
 import Card3 from '../images/card3.jpg';
 
-function Staff(title, body) {
+function Staff() {
     const text = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam vero aspernatur nemo reprehenderit magnam voluptatum repudiandae, itaque,`;
     return (
         <div className="container_staff">
@@ -16,18 +16,36 @@ function Staff(title, body) {
                     <h3 className="username">ZDZICHU Z CIEPLIC</h3>
                     <h5>Perkusista</h5>
                     <p>{text}</p>
+                    <a href="https://twitter.com">
+                        <Icon.Twitter className="profile_community" />
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <Icon.Instagram className="profile_community" />
+                    </a>
                 </div>
                 <div className="profile">
                     <img src={Card2} alt="Card2" className="profile_img" />
                     <h3 className="username">GADGA MESSLER</h3>
                     <h5>Wokalista</h5>
                     <p>{text}</p>
+                    <a href="https://twitter.com">
+                        <Icon.Twitter className="profile_community" />
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <Icon.Instagram className="profile_community" />
+                    </a>
                 </div>
                 <div className="profile">
                     <img src={Card3} alt="Card3" className="profile_img" />
                     <h3 className="username">EDZIU III WIELKI</h3>
                     <h5>Gitarzysta</h5>
                     <p>{text}</p>
+                    <a href="https://twitter.com">
+                        <Icon.Twitter className="profile_community" />
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <Icon.Instagram className="profile_community" />
+                    </a>
                 </div>
             </div>
         </div>
