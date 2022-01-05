4b1252b89ad0968f0c1c00ad8733702ca7d3fa5e
Ombre <krys1014@wp.pl>
2022-01-05 13:36:58 +0100

    Getting data from file Article.js and add mobile style

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import '../styles/Article.css';
+import '../styles/Articles.css';
+import { data } from '../data/Articles';
 
 function Articles() {
     const header = "Aktualnosci";
+
     return (
-        <div className="articles">
+        <div className="articles-container">
             <h1 className="header_article">{header}</h1>
-            <h4>asd</h4>
+            {data.map(({  id, title, date, image,  text }) => (
+                    <div key={id} className="article-container">
+                        <div className="left-side">
+                            <h3 className="article-title">{title}</h3>
+                            <img className="article-image" src={image} alt={id} />
+                        </div>
+                        <div className="right-side">
+                            <span className="article-date">{date}</span>
+                            <p className="article-text">{text}</p>
+                        </div>
+                    </div> 
+                ))}
         </div >
     );
 }
