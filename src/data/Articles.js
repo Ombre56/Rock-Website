5af400800237d3c5b68bbedf6a6df123358893d4
Ombre <krys1014@wp.pl>
2022-01-29 19:26:18 +0100

    Remove of .default

--- src/data/Articles.js
+++ src/data/Articles.js
@@ -3,28 +3,28 @@ export const data = [
         "id": 1,
         "title": "Koncert życzeń",
         "date": "24.05.2021",
-        "image": require("../images/article1.jpg").default,
+        "image": require("../assets/images/article1.jpg"),
         "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam vero aspernatur nemo reprehenderit magnam voluptatum repudiandae, itaque, asperiores accusantium molestiae hic? Ullam doloremque saepe totam dolorum aperiam esse sint. Nesciunt. Ipsum eum blanditiis error tempora, fugiat ullam non beatae minus nulla, aut quidem! Numquam vel voluptate facere tempora, error ea minima officia illum dolore excepturi cumque! Minus ducimus ut illo!"
     },
     {
         "id": 2,
         "title": "Spotkanie z fanami",
         "date": "5.06.2021",
-        "image": require("../images/article2.jpg").default,
+        "image": require("../assets/images/article2.jpg"),
         "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam vero aspernatur nemo reprehenderit magnam voluptatum repudiandae, itaque, asperiores accusantium molestiae hic? Ullam doloremque saepe totam dolorum aperiam esse sint. Nesciunt. Ipsum eum blanditiis error tempora, fugiat ullam non beatae minus nulla, aut quidem! Numquam vel voluptate facere tempora, error ea minima officia illum dolore excepturi cumque! Minus ducimus ut illo!"
     },
     {
         "id": 3,
         "title": "Sprzedaż koszulek",
         "date": "11.11.2021",
-        "image": require("../images/article1.jpg").default,
+        "image": require("../assets/images/article3.jpg"),
         "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam vero aspernatur nemo reprehenderit magnam voluptatum repudiandae, itaque, asperiores accusantium molestiae hic? Ullam doloremque saepe totam dolorum aperiam esse sint. Nesciunt. Ipsum eum blanditiis error tempora, fugiat ullam non beatae minus nulla, aut quidem! Numquam vel voluptate facere tempora, error ea minima officia illum dolore excepturi cumque! Minus ducimus ut illo!"
     },
     {
         "id": 4,
         "title": "Testowe ",
         "date": "19.01.2022",
-        "image": require("../images/article1.jpg").default,
+        "image": require("../assets/images/article1.jpg"),
         "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam vero aspernatur nemo reprehenderit magnam voluptatum repudiandae, itaque, asperiores accusantium molestiae hic? Ullam doloremque saepe totam dolorum aperiam esse sint. Nesciunt. Ipsum eum blanditiis error tempora, fugiat ullam non beatae minus nulla, aut quidem! Numquam vel voluptate facere tempora, error ea minima officia illum dolore excepturi cumque! Minus ducimus ut illo!"
     }
 ]
