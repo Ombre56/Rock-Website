038ab8222ccb9301cf3593fd24c86e734cea32ef
itzMaxinho <maxinho@kaem.net.pl>
2022-01-29 03:22:45 +0100

    Audio player added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Portfolio.css'
 
+import ReactAudioPlayer from 'react-audio-player';
+
 function Portfolio() {
+
     return (
         <div>
             <h3 className="header_portfolio">PORTFOLIO</h3>
@@ -16,17 +19,21 @@ function Portfolio() {
                         <p>Zespół Rockowy - Rozsądek za Żołądkiem</p></h1>
                 </div>
                 <div className="list2">
-                    <h1><p>Zespół Rockowy - McRollen Rock</p>
+                    <h1><p>Zespół Rockowy - Łamacz Nóg</p>
+                        <p>Zespół Rockowy - Dżemowy Leń</p>
+                        <p>Zespół Rockowy - Rozsądek za Żołądkiem</p>
+                        <p>Zespół Rockowy - McRollen Rock</p>
                         <p>Zespół Rockowy - Guitar Hero</p>
                         <p>Zespół Rockowy - Coś Miałem w Głowie</p>
-                        <p>Zespół Rockowy - Na Wieży Leży Jerzy</p>
-                        <p>Zespół Rockowy - Łamacz Nóg</p>
-                        <p>Zespół Rockowy - Dżemowy Leń</p>
-                        <p>Zespół Rockowy - Rozsądek za Żołądkiem</p></h1>
+                        <p>Zespół Rockowy - Na Wieży Leży Jerzy</p></h1>
                 </div>
-                <div className="player">
-                    <h6> Tu będzie audio player </h6>
-                </div>
+            </div>
+            <div className="container">
+                <ReactAudioPlayer
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"
+                    autoPlay
+                    controls
+                />
             </div>
         </div>
     )
