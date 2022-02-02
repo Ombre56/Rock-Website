83dc1c9db88b1543d8ae5a5bc075ebe6d3c32e29
Ombre <krys1014@wp.pl>
2022-02-02 18:44:47 +0100

    Remove autoPlay with ReactAudioPlayer

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -34,7 +34,7 @@ function Portfolio() {
             <div className="ReactAudioPlayer">
                 <ReactAudioPlayer
                     src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"
-                    autoPlay
+                    // autoPlay
                     controls
                 />
             </div>
